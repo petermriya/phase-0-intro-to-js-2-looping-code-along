fbd86b0cece4ae0d81bceacf2aaa5c0153547b0d
petermriya <petermuriya@gmail.com>
2022-09-01 11:50:18 +0300

    Attempted assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+const names = ['Peter', 'Brian', 'Sam'];
+function writeCards(names, _gift = 'suprise'){
+    let array = [];
+    for(let i = 0; i<names.length;i++){
+        array.push('Thank you, ${names[i]}, for the wonderful ${gift})')
+    }
+    return array;
+}
+const countDown = (num) => {
+    while(num >= 0){
+        console.log(num);
+        num --;
+    }
+}
